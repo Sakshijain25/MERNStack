deff2291aa2f1108d8d3f433fa6cce387bbb59a0
Sakshijain25 <43838707+Sakshijain25@users.noreply.github.com>
2023-03-04 14:36:50 +0530

    Initial Implementation

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import studentRoutes from './routes/student.js';
+
+
+const app = express();
+app.use('/students', studentRoutes);
+app.use(bodyParser.json({"limit": "20mb", "extended":true}));
+app.use(bodyParser.urlencoded({"limit": "20mb", "extended":true}));
+
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://khushboo:Khushboo2596@cluster0.9s4myqz.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => app.listen(PORT, () =>
+    console.log(`Connection is established and running on port: ${PORT}`)
+)).catch((err) => console.log(err.message));
+
+//mongoose.set('findOneAndReplace ', false);
+mongoose.set('strictQuery', true);
